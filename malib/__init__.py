bdaacad58b9603df466a69512c868f7b31b0f8f3
louisabraham <louis.abraham@yahoo.fr>
2024-06-10 13:49:17 +0200

    remove pytorch requirement

--- malib/__init__.py
+++ malib/__init__.py
@@ -1,7 +1,10 @@
 from .ratelimiter import RateLimiter
 from .exact_cover import exact_cover
-from .pytorch_bivariate_normal import (
-    standard_bivariate_normal_cdf,
-    bivariate_normal_cdf,
-)
-from .pytorch_interp import interp
+try:
+    from .pytorch_bivariate_normal import (
+        standard_bivariate_normal_cdf,
+        bivariate_normal_cdf,
+    )
+    from .pytorch_interp import interp
+except ModuleNotFoundError:
+    pass
